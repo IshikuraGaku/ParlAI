c90f2ae18a22cfb41fe05408751192d4af618916
IshikuraGaku <ig.fine518@gmail.com>
2019-06-20 18:20:53 +0900

    display_data_show

---
+++ examples/my_folder/wiki_display_data_show.py
@@ -0,0 +1,25 @@
+#入力と出力だけを取り出す
+#display_modelの出力を保存したファイル＝＞入出力だけのout.txt
+
+import re
+import argparse
+
+if __name__=="__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--input_file", default="input.txt", help="input_file_pass")
+    parser.add_argument("--output_file", default="output.txt", help="output_file_pass")
+    args=parser.parse_args()
+
+    with open(args.input_file, "r") as inputF, open(args.output_file, "w") as outputF:
+        inputTexts = inputF.read()
+        outputList1 = re.findall(r"\[wizard_of_wikipedia\]\:[^\[]*", inputTexts)
+        outputList2 = re.findall(r"\[EndToEnd\]\:[^\~]*", inputTexts)
+        #print(outputList[0]) ("[wizard_of_wikipedia]: Gardening\nI like Gardening, even when I've only been doing it for a short time.\n", '')
+        #print(outputList1)
+        #print(outputList2)
+        lines = ""
+        for line1, line2 in zip(outputList1,outputList2):
+             lines += line1
+             lines += line2
+        outputF.write(lines)
