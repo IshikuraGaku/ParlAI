53158c59231ff762dcc101621f10860d9f43e277
IshikuraGaku <ig.fine518@gmail.com>
2019-08-04 00:03:41 +0900

    less_memory

--- projects/my_wizard_of_wikipedia/generator/transformer/modules.py
+++ projects/my_wizard_of_wikipedia/generator/transformer/modules.py
@@ -484,13 +484,13 @@ class TransformerEncoder(nn.Module):
         tensor *= mask.unsqueeze(-1).type_as(tensor)
 
         if(self.act):
-            tensor, (remainders, n_updates) = self.act_fn(tensor, input, mask, self.enc, self.timing_embeddings, self.position_embeddings, self.n_layers)
+            tensor, (_, n_updates) = self.act_fn(tensor, input, mask, self.enc, self.timing_embeddings, self.position_embeddings, self.n_layers)
             #return tensor, (remainders, n_updates)
             n_update = n_updates.reshape(n_updates.shape[0]*n_updates.shape[1])
-            self.num += len(n_update)
-            self.num_of_layer_list = th.cat((self.num_of_layer_list, n_update))
-            average = self.num_of_layer_list.sum() / self.num
-            variance = ((self.num_of_layer_list - average) * (self.num_of_layer_list - average)).sum() / self.num
+            #self.num += len(n_update)
+            #self.num_of_layer_list = th.cat((self.num_of_layer_list, n_update))
+            #average = self.num_of_layer_list.sum() / self.num
+            #variance = ((self.num_of_layer_list - average) * (self.num_of_layer_list - average)).sum() / self.num
             """
             print("enc 平均層数")
             print(average)
@@ -722,11 +722,11 @@ class TransformerDecoder(nn.Module):
             """
             
             n_update = n_update.cpu().numpy()
-            self.num += 1
-            self.num_of_layer_list = np.append(self.num_of_layer_list, n_update[-1])
+            #self.num += 1
+            #self.num_of_layer_list = np.append(self.num_of_layer_list, n_update[-1])
 
-            average = self.num_of_layer_list.mean()
-            variance = ((self.num_of_layer_list - average) * (self.num_of_layer_list - average)).sum() / self.num
+            #average = self.num_of_layer_list.mean()
+            #variance = ((self.num_of_layer_list - average) * (self.num_of_layer_list - average)).sum() / self.num
             """
             print("dec 平均層数")
             print(average)
