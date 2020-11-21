17566290e29d3f6b1eca7687b0e835b85146d1ce
IshikuraGaku <ig.fine518@gmail.com>
2020-11-21 22:47:36 +0900

    act_loss

--- parlai/core/torch_agent.py
+++ parlai/core/torch_agent.py
@@ -1792,7 +1792,7 @@ class TorchAgent(ABC, Agent):
         if self.fp16:
             self.optimizer.backward(loss, update_master_grads=False)
         else:
-            loss.backward()
+            loss.backward(retain_graph=True)
 
     def update_params(self):
         """
