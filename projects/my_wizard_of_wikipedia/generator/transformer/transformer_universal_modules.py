dc2ba395303ea45f6bb723f563841d449add715f
IshikuraGaku <ig.fine518@gmail.com>
2020-11-25 20:20:12 +0900

    act_l2

--- projects/my_wizard_of_wikipedia/generator/transformer/transformer_universal_modules.py
+++ projects/my_wizard_of_wikipedia/generator/transformer/transformer_universal_modules.py
@@ -1296,7 +1296,7 @@ class UniversalTransformerEncoder(nn.Module):
             self.act_loss = th.mean(remainders + n_updates)
             if self.act_l2:
                 tmp_act_l2_loss = None
-                for param in self.act_fn.parameters:
+                for param in self.act_fn.parameters():
                     if tmp_act_l2_loss is None:
                         tmp_act_l2_loss = th.norm(param)
                     else:
@@ -1543,7 +1543,7 @@ class UniversalTransformerDecoder(nn.Module):
             self.act_loss = th.mean(remainders + n_updates)
             if self.act_l2:
                     tmp_act_l2_loss = None
-                for param in self.act_fn.parameters:
+                for param in self.act_fn.parameters():
                     if tmp_act_l2_loss is None:
                         tmp_act_l2_loss = th.norm(param)
                     else:
