45e9f5a68afa4627b4dedd359ad46a6a28b7d7c0
IshikuraGaku <ig.fine518@gmail.com>
2020-11-28 20:19:12 +0900

    light_act

--- projects/my_wizard_of_wikipedia/generator/transformer/transformer_universal_modules.py
+++ projects/my_wizard_of_wikipedia/generator/transformer/transformer_universal_modules.py
@@ -2621,26 +2621,26 @@ class ACT_Light(nn.Module):
         return previous_tensor, (remainders, n_updates)
     
     def universal_sentence_embedding(sentences, mask, sqrt=True, use_mask=True):
+            """
+        Perform Universal Sentence Encoder averaging (https://arxiv.org/abs/1803.11175).
+
+        This is really just sum / sqrt(len).
+
+        :param Tensor sentences: an N x T x D of Transformer outputs. Note this is
+            the exact output of TransformerEncoder, but has the time axis first
+        :param ByteTensor: an N x T binary matrix of paddings
+
+        :return: an N x D matrix of sentence embeddings
+        :rtype Tensor:
         """
-    Perform Universal Sentence Encoder averaging (https://arxiv.org/abs/1803.11175).
-
-    This is really just sum / sqrt(len).
-
-    :param Tensor sentences: an N x T x D of Transformer outputs. Note this is
-        the exact output of TransformerEncoder, but has the time axis first
-    :param ByteTensor: an N x T binary matrix of paddings
-
-    :return: an N x D matrix of sentence embeddings
-    :rtype Tensor:
-    """
-    # need to mask out the padded chars
-    sentence_sums = th.bmm(
-        sentences.permute(0, 2, 1),
-        mask.float().unsqueeze(-1)
-    ).squeeze(-1)
-    if use_mask:
-        divisor = mask.sum(dim=1).view(-1, 1).float()
-        if sqrt:
-            divisor = divisor.sqrt()
-        sentence_sums = sentence_sums / divisor
-    return sentence_sums
+        # need to mask out the padded chars
+        sentence_sums = th.bmm(
+            sentences.permute(0, 2, 1),
+            mask.float().unsqueeze(-1)
+        ).squeeze(-1)
+        if use_mask:
+            divisor = mask.sum(dim=1).view(-1, 1).float()
+            if sqrt:
+                divisor = divisor.sqrt()
+            sentence_sums = sentence_sums / divisor
+        return sentence_sums
