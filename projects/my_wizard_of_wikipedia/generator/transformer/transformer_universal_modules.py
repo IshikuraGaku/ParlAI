1728c9c35b45aaf72fbd8d3bb7e66954d8b41822
IshikuraGaku <ig.fine518@gmail.com>
2020-11-29 01:09:34 +0900

    a

--- projects/my_wizard_of_wikipedia/generator/transformer/transformer_universal_modules.py
+++ projects/my_wizard_of_wikipedia/generator/transformer/transformer_universal_modules.py
@@ -2643,11 +2643,11 @@ class ACT_Light(nn.Module):
 
         # need to mask out the padded chars
         #sentences = [B,L,emb]
-        #sentences = sentences.permute(0, 2, 1)
+        sentences = sentences.permute(0, 2, 1)
         mask_tmp = mask.float().unsqueeze(-1) 
         print(sentences.shape)
         print(mask_tmp.shape)
-        sentence_sums = th.bmm(sentences, mask_tmp)
+        sentence_sums = sentences * mask_tmp
 
         sentemces_sums = sentences_sums.squeeze(-1)
         if use_mask:
